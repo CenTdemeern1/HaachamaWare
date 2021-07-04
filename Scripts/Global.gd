19325f86fcf7aab0f4f7e54a38e65883b9ac7ae2
CenTdemeern1 <timo.herngreen@gmail.com>
2021-07-04 20:22:13 +0200

    Demo sequence!

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var resource_queue
+var fadeinMM = true
 
 func change_scene(to):
 	get_tree().change_scene_to(resource_queue.get_resource(to))
--- Scripts/MainScene.gd
+++ Scripts/MainScene.gd
@@ -1,14 +1,23 @@
 extends Node2D
 
+var demotimer = -1
 var can_press_start = false
 
 func _ready():
-	modulate=Color(0,0,0)
+	if Global.fadeinMM:
+		modulate=Color(0,0,0)
 
 
 func _process(delta):
 	if !modulate.r>=1:
 		modulate=Color(modulate.r+delta*4,modulate.g+delta*4,modulate.b+delta*4)
+	if demotimer >= 0:
+		demotimer+=delta
+	else:
+		if can_press_start:
+			demotimer=delta
+	if demotimer >= 30:
+		Global.change_scene("res://Scenes/Demo.tscn")
 	if can_press_start:
 		if Input.is_action_just_pressed("start"):
 			can_press_start=false
