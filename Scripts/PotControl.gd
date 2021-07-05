9de4be1af56d5f6eb9914217ec419b4c09ff9eed
CenTdemeern1 <timo.herngreen@gmail.com>
2021-07-05 12:51:07 +0200

    You can now press start to go back to the main menu after a game over

--- Scripts/PotControl.gd
+++ Scripts/PotControl.gd
@@ -1,8 +1,13 @@
 extends AnimationPlayer
 
 var start_minigame_timer = 5
+var gameover = false
+var gameoverfinished = false
 
 func _process(delta):
+	if gameoverfinished:
+		if Input.is_action_just_pressed("start"):
+			Global.change_scene("res://Scenes/MainScene.tscn")
 	if start_minigame_timer<3:
 		start_minigame_timer+=delta
 		if start_minigame_timer>=3:
@@ -17,6 +22,9 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 		$"../../..".unload_minigame()
 		if $"../../..".HP==0:
 			play("GameOver")
+			gameover = true
 		else:
 			play("Bad")
 			start_minigame_timer = 0
+	if anim_name=="GameOver":
+		gameoverfinished=true
