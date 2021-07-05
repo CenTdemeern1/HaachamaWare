52236b9d9ecc7b1cc8c3973bead9dd36228ca627
CenTdemeern1 <timo.herngreen@gmail.com>
2021-07-05 13:50:14 +0200

    Confine crosshair to playfield

--- Scripts/MicroGames/Crosshair.gd
+++ Scripts/MicroGames/Crosshair.gd
@@ -14,6 +14,9 @@ func _process(delta):
 	if !shot:
 		position.x+=(Input.get_action_strength("right")-Input.get_action_strength("left"))*delta*480
 		position.y+=(Input.get_action_strength("down")-Input.get_action_strength("up"))*delta*480
+		var clamprect : Rect2 = $"../ReferenceRect".get_rect()
+		position.x = clamp(position.x,clamprect.position.x,clamprect.position.x+clamprect.size.x)
+		position.y = clamp(position.y,clamprect.position.y,clamprect.position.y+clamprect.size.y)
 
 
 func _on_Crosshair_animation_finished():
