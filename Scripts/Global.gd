1d9eb45e6bb5e294b6cb8366c72b55cb105c2133
CenTdemeern1 <timo.herngreen@gmail.com>
2021-07-08 16:50:21 +0200

    Improve RNG to make it (seem) less deterministic for the user
    Still TAS-able though, which is the intention. No outside influences like system time are going to influence the RNG here.

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -8,3 +8,17 @@ func change_scene(to):
 
 func get_instance(name):
 	return resource_queue.get_resource(name).instance()
+
+func _process(_delta):
+	#RNG scrambling.
+	#TAS-able. (RNG derived only from timing and user input)
+	advance_RNG()
+	for action in InputMap.get_actions():
+		if Input.is_action_pressed(action):
+			advance_RNG(3)
+
+func advance_RNG(a=null):
+	if a==null:
+		a=1
+	for i in range(a):
+		randi()
