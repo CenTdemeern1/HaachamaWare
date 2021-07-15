e297ca9c24d49e33b63d657ad6f4cb9cc82670f3
CenTdemeern1 <timo.herngreen@gmail.com>
2021-07-15 13:58:47 +0200

    Tank level smoothly decreases on button press

--- Scripts/MicroGames/CalliopeTankControl.gd
+++ Scripts/MicroGames/CalliopeTankControl.gd
@@ -1,6 +1,7 @@
 extends AnimatedSprite
 
 var tank_value : float = 1.0 setget set_tank_value,get_tank_value
+var goal = 1.0
 
 func _ready():
 	play()
@@ -15,7 +16,10 @@ func set_tank_value(v:float):
 		tank.value=round(tank_value*tank.rect_size.y)/tank.rect_size.y
 
 func _process(delta):
-	self.tank_value-=delta*0.1
+	if Input.is_action_just_pressed("button1"):
+#		print("beep!",self.tank_value)
+		goal -= 0.1
+	self.tank_value-=(self.tank_value-goal)*.25
 	if frame==0:
 		$Tanks.rect_position.y=0
 	if frame==1:
