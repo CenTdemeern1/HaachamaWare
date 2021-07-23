7725a16287f138a2a95c02886d641a4ae6832c56
CenTdemeern1 <timo.herngreen@gmail.com>
2021-07-23 13:30:52 +0200

    Fix bug where doors would open multiple times

--- Scripts/MicroGames/SuiseiAmongUs.gd
+++ Scripts/MicroGames/SuiseiAmongUs.gd
@@ -2,6 +2,7 @@ extends MicroGame
 
 func _process(_delta):
 	if timer <= 1 and !preemptively_won_or_lost:
+		lose()
 		$SuiseiImpostor.set_process(false)
 		$AnimationPlayer.playback_speed=$"..".difficulty
 		$AnimationPlayer.play("Lose")
