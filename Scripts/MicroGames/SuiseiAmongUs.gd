11832b471a7082e422952c536d261e40517ff2da
CenTdemeern1 <timo.herngreen@gmail.com>
2021-08-04 13:52:54 +0200

    [SuiseiAmongus] Extend laugh and fix animation lock

--- Scripts/MicroGames/SuiseiAmongUs.gd
+++ Scripts/MicroGames/SuiseiAmongUs.gd
@@ -4,5 +4,7 @@ func _process(_delta):
 	if timer <= 1 and !preemptively_won_or_lost:
 		lose()
 		$SuiseiImpostor.set_process(false)
+		$SuiseiImpostor.playing=false
+		$SuiseiImpostor.frame=0
 		$AnimationPlayer.playback_speed=$"..".difficulty
 		$AnimationPlayer.play("Lose")
