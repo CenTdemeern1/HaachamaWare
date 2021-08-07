80235f86fde85636e9e48a9cd22a04d668e8bacd
CenTdemeern1 <timo.herngreen@gmail.com>
2021-08-07 16:39:28 +0200

    Change difficulty curve for Evolving at Xerunox's request
    Co-Authored-By: Xerunox <66562838+xerunox@users.noreply.github.com>

--- Scripts/MicroGames/Evolving.gd
+++ Scripts/MicroGames/Evolving.gd
@@ -3,22 +3,26 @@ extends MicroGame
 var generaltimer = 0
 var mashcount = 0
 var played_booing = false
+var difficulty
 
 func _ready():
+	difficulty = clamp(1+(($"..".difficulty-1)*0.5),1,1.5)
+	lifetime/=difficulty
+	timer = lifetime
 	$Evolve.show()
 	$EvolveCancel.hide()
-	$Evolve.speed_scale=$"..".difficulty
-	$EvolveCancel.speed_scale=$"..".difficulty
+	$Evolve.speed_scale=difficulty
+	$EvolveCancel.speed_scale=difficulty
 	$Evolve.play()
 	$EvolveCancel.play()
 
 func _process(delta):
 	generaltimer+=delta
-	if generaltimer>=2.0/$"..".difficulty and !played_booing and !preemptively_won_or_lost:
+	if generaltimer>=2.0/difficulty and !played_booing and !preemptively_won_or_lost:
 		played_booing=true
 		$Booing.play()
-	if generaltimer>0.3/$"..".difficulty:
-		if generaltimer<2.0/$"..".difficulty:
+	if generaltimer>0.3/difficulty:
+		if generaltimer<2.0/difficulty:
 			if Input.is_action_just_pressed("button2"):
 				mashcount+=1
 				if mashcount == 4:
