1f0c9755c7a264a778e448e58d1770f43dbf620b
CenTdemeern1 <timo.herngreen@gmail.com>
2021-10-17 22:52:21 +0200

    Add a quick TODO note to the Chicken

--- Scripts/MicroGames/Cjhikin.gd
+++ Scripts/MicroGames/Cjhikin.gd
@@ -1,5 +1,7 @@
 extends AnimatedSprite
 
+#TODO: Have the chicken take the highest Z value of the pegs above it, +1, and set it as it's own Z value
+
 func _process(delta):
 #	var x = $"../Navigation2D".get_simple_path(self.position,get_global_mouse_position())
 	var x = $"../Navigation2D".get_closest_point(get_global_mouse_position())
