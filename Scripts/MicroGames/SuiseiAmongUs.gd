ca3117a5e62f579e15f7704a3cf86ec4fdaa4692
CenTdemeern1 <timo.herngreen@gmail.com>
2022-01-21 23:24:12 +0100

    [Read desc] Change bomb timer behavior in SuiseiAmongUs
    Changed the bomb timer behavior to more accurately represent the gameplay time left instead of it including the "cutscene time" for when you lose. The cutscene is played after the timer ends, and the minigame ends right after.
    The minigame is ended immediately on time over when won, since it'd be stupid to waste time staring at a cutscene not happening. Especially since the timer has already ended by then.

--- Scripts/MicroGames/SuiseiAmongUs.gd
+++ Scripts/MicroGames/SuiseiAmongUs.gd
@@ -1,8 +1,22 @@
 extends MicroGame
 
-func _process(_delta):
-	if timer <= 1 and !preemptively_won_or_lost:
+var count_endtimer = false
+var endtimer = 0
+
+func _process(delta):
+	if count_endtimer:
+		endtimer+=delta
+		if endtimer>=1:
+			end()
+#	if timer <= 1 and !preemptively_won_or_lost:
+
+func time_over():
+	if preemptively_won_or_lost:
+		if !count_endtimer:
+			end()
+	else:
 		lose()
+		count_endtimer = true
 		$SuiseiImpostor.set_process(false)
 		$SuiseiImpostor.playing=false
 		$SuiseiImpostor.frame=0
